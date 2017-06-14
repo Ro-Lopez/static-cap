97f0e72358eadbda6ad6c424bef1f431f025e971
Robbie Lopez <rolopez.email@gmail.com>
2017-06-14 11:18:52 -0600

    added angular profile temp

--- index.php
+++ index.php
@@ -2,82 +2,120 @@
 
 	<head>
 		<title>Pet Foster ABQ Sign-In</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--		<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!--		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">-->
 
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+
+<!--		<link rel="stylesheet" href="css/style.css">-->
 
 		<!-- jQuery library -->
+
+
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!--		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>-->
+<!---->
+<!--		<script src="https://code.jquery.com/jquery.js"></script>-->
+<!---->
+<!--		<script src="css/style.css"></script>-->
 
-		<script src="https://code.jquery.com/jquery.js"></script>
 
-		<script src="css/style.css"></script>
 	</head>
 
 	<body>
 
 		<h2>Pet Rescue ABQ Sign In</h2>
 
-		<!-- Trigger/Open The Modal -->
-		<button id="myBtn">Hey! Sign In!</button>
+<!--		<!-- Trigger/Open The Modal -->
+<!--		<button id="myBtn">Hey! Sign In!</button>-->
+<!---->
+<!--		<!-- The Modal -->
+<!--		<div id="myModal" class="modal">-->
+<!---->
+<!--			<!-- Modal content -->
+<!--			<div class="modal-content">-->
+<!--				<div class="modal-header">-->
+<!--					<span class="close">&times;</span>-->
+<!--					<h2>Pet Resecue ABQ Header</h2>-->
+<!--				</div>-->
+<!--				<div class="modal-body">-->
+<!---->
+<!---->
+<!--					<div class="container">-->
+<!--						<h2 align="center">Authentication</h2>-->
+<!--						<div class="center">-->
+<!--							<form method="post" action="/signin" class="form-horizontal" role="form" align="center">-->
+<!--								<div class="form-group" align="center">-->
+<!--									<label class="control-label col-sm-2" for="username">Username:<em>*</em></label>-->
+<!--									<div class="col-sm-6">-->
+<!--										<input type="text" name="username" id="username" placeholder="username" required="true" class="form-control" />-->
+<!--									</div>-->
+<!--								</div>-->
+<!--								<div class="form-group">-->
+<!--									<label class="control-label col-sm-2" for="password">Password:<em>*</em></label>-->
+<!--									<div class="col-sm-6">-->
+<!--										<input type="password" name="password" id="password" required="true" class="form-control" />-->
+<!--									</div>-->
+<!--								</div>-->
+<!--								<div class="form-group">-->
+<!--									<div class="col-sm-offset-2 col-sm-8">-->
+<!--										<input type="submit" name="signin" id="signin" value="Sign In" class="btn btn-default" />-->
+<!--									</div>-->
+<!--								</div>-->
+<!--							</form>-->
+<!--						</div>-->
+<!--					</div>-->
+<!---->
+<!---->
+<!---->
+<!--				</div>-->
+<!--				<div class="modal-footer">-->
+<!--					<h3>Footer</h3>-->
+<!--				</div>-->
+<!--			</div>-->
+<!---->
+<!--		</div>-->
+<!---->
 
-		<!-- The Modal -->
-		<div id="myModal" class="modal">
 
-			<!-- Modal content -->
-			<div class="modal-content">
-				<div class="modal-header">
-					<span class="close">&times;</span>
-					<h2>Pet Resecue ABQ Header</h2>
-				</div>
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+			Launch demo modal
+		</button>
 
-				<form #signInForm="ngForm" name="signInForm" id="signInForm"
-						(ngSubmit)="signIn();">
-
-				<div class="modal-body">
-					<div class="container">
-						<h2 align="center">Authentication</h2>
-						<div class="center">
-							<form method="post" action="/signin" class="form-horizontal" role="form" align="center">
-								<div class="form-group" align="center">
-									<label class="control-label col-sm-2" for="email">Email:<em>*</em></label>
-									<div class="col-sm-6">
-
-										<input type="text" name="email" id="email" placeholder="email" required [(ngModel)]="signIn.profileEmail" #profileEmail="ngModel" class="form-control" />
-
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="control-label col-sm-2" for="password">Password:<em>*</em></label>
-									<div class="col-sm-6">
-
-										<input type="password" name="password" id="password" required [(ngModel)]="signIn.profilePassword" #profilePassword="ngModel" class="form-control" />
-
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="col-sm-offset-2 col-sm-8">
-										<input type="submit" name="signin" id="signin" [disabled]="signInForm.invalid" value="Sign In" class="btn btn-default" />
-									</div>
-								</div>
-							</form>
-						</div>
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
 					</div>
-
-
-
-				</div>
-				<div class="modal-footer">
-					<h3>Footer</h3>
+					<div class="modal-body">
+						...
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Save changes</button>
+					</div>
 				</div>
 			</div>
-
 		</div>
+
+
+
+
+
+
+
 
 
 
@@ -86,32 +124,32 @@
 
 </html>
 
-
-
-<script>
-	// Get the modal
-	var modal = document.getElementById('myModal');
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
-
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks the button, open the modal
-	btn.onclick = function() {
-		modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	}
-</script>
+<!---->
+<!---->
+<!--<script>-->
+<!--	// Get the modal-->
+<!--	var modal = document.getElementById('myModal');-->
+<!---->
+<!--	// Get the button that opens the modal-->
+<!--	var btn = document.getElementById("myBtn");-->
+<!---->
+<!--	// Get the <span> element that closes the modal-->
+<!--	var span = document.getElementsByClassName("close")[0];-->
+<!---->
+<!--	// When the user clicks the button, open the modal-->
+<!--	btn.onclick = function() {-->
+<!--		modal.style.display = "block";-->
+<!--	}-->
+<!---->
+<!--	// When the user clicks on <span> (x), close the modal-->
+<!--	span.onclick = function() {-->
+<!--		modal.style.display = "none";-->
+<!--	}-->
+<!---->
+<!--	// When the user clicks anywhere outside of the modal, close it-->
+<!--	window.onclick = function(event) {-->
+<!--		if (event.target == modal) {-->
+<!--			modal.style.display = "none";-->
+<!--		}-->
+<!--	}-->
+<!--</script>-->
